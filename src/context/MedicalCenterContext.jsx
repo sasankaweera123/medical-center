f786cc8a1499af32e6c8686733b6f3f9b891e712
SasankaWeera <sasanka.weerakoon@aiken.com.lk>
2024-03-26 12:34:53 +0530

    - add a new logout function - update according to the template

--- src/context/MedicalCenterContext.jsx
+++ src/context/MedicalCenterContext.jsx
@@ -1,10 +1,11 @@
 import {createContext, useCallback, useEffect, useMemo, useState} from "react";
+import {TokenUser} from "../dto/TokenUser";
 
 export const MedicalCenterContext = createContext(null);
 
 const MedicalCenterContextProvider = (props) => {
 
-    const [user, setUser] = useState({});
+    const [user, setUser] = useState(TokenUser);
 
     useEffect(() => {
         const user = localStorage.getItem('user');
@@ -17,12 +18,18 @@ const MedicalCenterContextProvider = (props) => {
         localStorage.setItem('user', JSON.stringify(user));
     }, []);
 
+    const logout = useCallback(() => {
+        localStorage.removeItem('user');
+        setUser(TokenUser);
+    }, []);
+
     const contextValue = useMemo(() => {
         return {
             user,
-            setUserContext
+            setUserContext,
+            logout
         }
-    }, [user, setUserContext]);
+    }, [user, setUserContext, logout]);
     return (
         <MedicalCenterContext.Provider value={contextValue}>
             {props.children}
